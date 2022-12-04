6e69ae8cd81e3089bcfc7749ac9d99cc0a582934
Thomas Rauhofer <pommi15@hotmail.com>
2022-12-04 11:43:44 +0100

    refactor(day1)

--- src/day1/day1.ts
+++ src/day1/day1.ts
@@ -7,23 +7,16 @@ export class Day1 extends Day {
   }
 
   public part1(): number {
-    let counter: number = 0;
-    const elves: number[] = [];
-    for (const n of this.data) {
-      const num = Number(n);
-      if (num) {
-        counter += num;
-      } else {
-        elves.push(counter);
-        counter = 0;
-      }
-    }
-    elves.push(counter);
-    const finalElves = elves.sort((a, b) => b - a);
+    const finalElves = this.createFinalElves();
     return finalElves[0];
   }
 
   public part2(): number {
+    const finalElves = this.createFinalElves();
+    return finalElves.filter((_, i) => i < 3).reduce((p, c) => p + c, 0);
+  }
+
+  private createFinalElves(): number[] {
     let counter: number = 0;
     const elves: number[] = [];
     for (const n of this.data) {
@@ -36,7 +29,6 @@ export class Day1 extends Day {
       }
     }
     elves.push(counter);
-    const finalElves = elves.sort((a, b) => b - a);
-    return finalElves[0] + finalElves[1] + finalElves[2];
+    return elves.sort((a, b) => b - a);
   }
 }
